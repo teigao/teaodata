3786858dd445c474f8bc3124669e40c284cd96d2
teigao <yiqgao@outlook.com>
2019-12-18 11:46:10 +0800

    add visual

---
+++ teaodata/visual_tool/__init__.py
@@ -0,0 +1,2 @@
+import os
+os.system('"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe" C:\\Users\\yiqga\\OneDrive\\WorkSync\\teaodata\\teaodata\\visual_tool\\_config\\sample.html')
