9cd1d54bcf7e77b7fc1f0c39bb874ea86d4bb147
teigao <yiqgao@outlook.com>
2019-12-27 10:13:21 +0800

    edit visual html open function

--- teaodata/visual_tool/__init__.py
+++ teaodata/visual_tool/__init__.py
@@ -1,2 +1,11 @@
 import os
-os.system('"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe" C:\\Users\\yiqga\\OneDrive\\WorkSync\\teaodata\\teaodata\\visual_tool\\_config\\sample.html')
+import webbrowser
+
+
+_current_path = os.path.dirname(__file__)
+_project_root_folder = _current_path+"/_config/"
+
+
+html_path = _project_root_folder + 'sample.html'
+
+webbrowser.open_new_tab(html_path)
